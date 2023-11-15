6ffbda6662d3a4d3c8456b8c26c9780fadac3784
AleksandarSapic <shapke10@gmail.com>
2023-11-15 12:25:05 +0100

    Implemented showing list of dragged blocks

--- src/components/main right side/DraggedBlocksList.tsx
+++ src/components/main right side/DraggedBlocksList.tsx
@@ -1,8 +1,17 @@
+import DraggedBlocks from "../../data/DraggedBlocks";
+
 function DraggedBlocksList() {
   return (
     <>
       <div className="d-flex flex-column dragged-blocks-list">
-        <p className="text-center">Blokovi</p>
+        <span className="text-center">Blokovi</span>
+        {DraggedBlocks.map((block) => (
+          <div key={block.id} className="one-dragged-block-section">
+            <span>{block.id}</span>
+            <span>. </span>
+            <span>{block.name}</span>
+          </div>
+        ))}
       </div>
     </>
   );
---
+++ src/data/DraggedBlocks.tsx
@@ -0,0 +1,8 @@
+const DraggedBlocks = [
+  { id: 1, name: "ArcTan" },
+  { id: 2, name: "Sign" },
+  { id: 3, name: "Cos" },
+  { id: 4, name: "Mrtva zona" },
+];
+
+export default DraggedBlocks;
