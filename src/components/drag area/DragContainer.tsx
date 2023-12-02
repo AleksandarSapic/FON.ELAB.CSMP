7573978ad122c3c333d4e2c7ccdaae387916a56d
AleksandarSapic <shapke10@gmail.com>
2023-12-02 18:17:13 +0100

    Implemented function for setting selected block on dragged block click

--- src/components/drag area/DragContainer.tsx
+++ src/components/drag area/DragContainer.tsx
@@ -2,15 +2,30 @@ import { useContext } from "react";
 import { Stage, Layer, Rect, Text, Label, Line } from "react-konva";
 
 import DraggedBlocksContext from "../../hooks/DraggedBlocksContext";
+import SetSelectedDraggedBlockContext from "../../hooks/SetSelectedDraggedBlockContext";
 
 function DragContainer() {
   const blocks = useContext(DraggedBlocksContext);
+  const callSetSelectedBlock = useContext(SetSelectedDraggedBlockContext);
+
+  function setSelectedBlock(id: number) {
+    const block = blocks.find((block) => block.id === id);
+    if (block !== undefined) {
+      callSetSelectedBlock(block);
+    }
+  }
 
   return (
     <Stage width={1000} height={1000}>
       <Layer>
         {blocks.map((block) => (
-          <Label key={block.id} x={block.x} y={block.y} draggable>
+          <Label
+            key={block.id}
+            x={block.x}
+            y={block.y}
+            draggable
+            onClick={() => setSelectedBlock(block.id)}
+          >
             <Rect
               width={100}
               height={60}
