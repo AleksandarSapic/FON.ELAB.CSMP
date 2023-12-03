0949b5d243807bdfdf6531a857820b4277d126a3
AleksandarSapic <shapke10@gmail.com>
2023-12-03 20:39:54 +0100

    Enabled functionality to drop on scrolled div, with right location

--- src/components/drag area/DragArea.tsx
+++ src/components/drag area/DragArea.tsx
@@ -1,24 +1,36 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import DragContainer from "./DragContainer";
 
 import DraggingBlockContext from "../../hooks/DraggingBlockContext";
 import HandleDrop from "../../hooks/HandleDropContext";
 
 function DragArea() {
+  const [scrollX, setScrollX] = useState(0);
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
+    const currentScrollX = event.currentTarget.scrollLeft;
+    const currentScrollY = event.currentTarget.scrollTop;
+
+    setScrollX(currentScrollX);
+    setScrollY(currentScrollY);
+  };
   const draggingBlock = useContext(DraggingBlockContext);
   const handleDrop = useContext(HandleDrop);
+
   return (
     <div
       className="position-relative w-50 drag-area"
       onDrop={(e: React.DragEvent) => {
         e.preventDefault();
-        const x = e.clientX - window.innerWidth * 0.25; //Sirina main left side sekcije
-        const y = e.clientY - 70; //Visina header sekcije
+        const x = e.clientX - window.innerWidth * 0.25 + scrollX; //Sirina main left side sekcije i pozicija diva nakon scrolla po X
+        const y = e.clientY - 70 + scrollY; //Visina header sekcije i pozicija diva nakon scrolla po Y
         handleDrop(draggingBlock.name, x, y);
       }}
       onDragOver={(e: React.DragEvent) => {
         e.preventDefault();
       }}
+      onScroll={handleScroll}
     >
       <DragContainer />
     </div>
