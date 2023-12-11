4fb76e3220f31c2a7c43a74ef5fedaed82ba323b
AleksandarSapic <shapke10@gmail.com>
2023-12-11 11:08:55 +0100

    Deleted an unused component

--- src/components/drag area/DraggedBlock.tsx
+++
@@ -1,7 +0,0 @@
-import IBlockProps from "../../interfaces/IBlockProps";
-
-function DraggedBlock({ name }: IBlockProps) {
-  return <div className="dragged-block">{name}</div>;
-}
-
-export default DraggedBlock;
